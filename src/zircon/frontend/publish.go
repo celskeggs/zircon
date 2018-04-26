b8421708d9ba813806358dc968c720db377dc077
Cel A. Skeggs <cela@mit.edu>
2018-04-26 16:55:13 -0400

    Slightly centralize RPC

--- src/zircon/frontend/publish.go
+++ src/zircon/frontend/publish.go
@@ -2,17 +2,6 @@ package frontend
 
 import "zircon/apis"
 
-// Starts serving an RPC handler for a Frontend on a certain address. Runs forever.
-func PublishFrontend(server apis.Frontend, address apis.ServerAddress) error {
-	panic("unimplemented")
-}
-
-// Subscribes to a frontend RPC server over the network on a specific address.
-// Failure to connect does *not* cause an error here; just timeouts when trying to call specific methods.
-func SubscribeFrontend(address apis.ServerAddress) (apis.Frontend, error) {
-	panic("unimplemented")
-}
-
 // Constructs a frontend that can connect to the network
 func ConstructFrontendOnNetwork(local apis.ServerAddress, etcd apis.EtcdInterface,
 		localMetadata apis.MetadataCache) (apis.Frontend, error) {
--- src/zircon/rpc/publish.go
+++ src/zircon/rpc/publish.go
@@ -11,6 +11,10 @@ type ConnectionCache interface {
 	// Failure to connect does *not* cause an error here; just timeouts when trying to call specific methods.
 	SubscribeChunkserver(address apis.ServerAddress) (apis.Chunkserver, error)
 
+	// Subscribes to a frontend RPC server over the network on a specific address.
+	// Failure to connect does *not* cause an error here; just timeouts when trying to call specific methods.
+	SubscribeFrontend(address apis.ServerAddress) (apis.Frontend, error)
+
 	CloseAll()
 }
 
@@ -20,5 +24,10 @@ func NewConnectionCache() (ConnectionCache, error) {
 
 // Starts serving an RPC handler for a Chunkserver on a certain address. Runs forever.
 func PublishChunkserver(server apis.Chunkserver, address string) (control.Teardown, apis.ServerAddress, error) {
+	panic("unimplemented")
+}
+
+// Starts serving an RPC handler for a Frontend on a certain address. Runs forever.
+func PublishFrontend(server apis.Frontend, address apis.ServerAddress) error {
 	panic("unimplemented")
 }
