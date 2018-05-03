12627ed25c7b2f1f47f253712aa973bccaccdb74
Cel A. Skeggs <cela@mit.edu>
2018-05-03 12:30:39 -0400

    Remove extraneous assertions instance from chunkserver_test

--- src/zircon/rpc/chunkserver_test.go
+++ src/zircon/rpc/chunkserver_test.go
@@ -2,26 +2,24 @@ package rpc
 
 import (
 	"errors"
-	testifyAssert "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
 	"zircon/apis"
 )
 
-func beginTest(t *testing.T) (*testifyAssert.Assertions, *MockChunkserver, func(), apis.Chunkserver) {
-	assert := testifyAssert.New(t)
-
+func beginTest(t *testing.T) (*MockChunkserver, func(), apis.Chunkserver) {
 	cache := NewConnectionCache()
 
 	mocked := new(MockChunkserver)
 
 	teardown, address, err := PublishChunkserver(mocked, ":0")
-	assert.NoError(err)
+	assert.NoError(t, err)
 
 	server, err := cache.SubscribeChunkserver(address)
-	assert.NoError(err)
+	assert.NoError(t, err)
 
-	return assert, mocked, func() {
+	return mocked, func() {
 		mocked.AssertExpectations(t)
 
 		teardown(true)
@@ -30,7 +28,7 @@ func beginTest(t *testing.T) (*testifyAssert.Assertions, *MockChunkserver, func(
 }
 
 func TestChunkserver_StartWriteReplicated(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("StartWriteReplicated", 73, 55, []byte("this is a hello\000 world!!\n"),
@@ -40,117 +38,117 @@ func TestChunkserver_StartWriteReplicated(t *testing.T) {
 
 	err := server.StartWriteReplicated(73, 55, []byte("this is a hello\000 world!!\n"),
 		[]apis.ServerAddress{"abc", "def", "ghi.mit.edu"})
-	assert.NoError(err)
+	assert.NoError(t, err)
 
 	err = server.StartWriteReplicated(0, 0, []byte("|||"), []apis.ServerAddress{})
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 01")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 01")
 }
 
 func TestChunkserver_Replicate(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("Replicate", 74, "jkl.mit.edu", 56).Return(nil)
 	mocked.On("Replicate", 0, "", 0).Return(errors.New("hello world 02"))
 
-	assert.NoError(server.Replicate(74, "jkl.mit.edu", 56))
+	assert.NoError(t, server.Replicate(74, "jkl.mit.edu", 56))
 
 	err := server.Replicate(0, "", 0)
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 02")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 02")
 }
 
 func TestChunkserver_Read(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("Read", 75, 57, 58, 59).Return("testy testy", 60, nil)
 	mocked.On("Read", 0, 0, 0, 0).Return("", 6, errors.New("hello world 03"))
 
 	data, ver, err := server.Read(75, 57, 58, 59)
-	assert.NoError(err)
-	assert.Equal("testy testy", string(data))
-	assert.Equal(apis.Version(60), ver)
+	assert.NoError(t, err)
+	assert.Equal(t, "testy testy", string(data))
+	assert.Equal(t, apis.Version(60), ver)
 
 	_, ver, err = server.Read(0, 0, 0, 0)
-	assert.Error(err)
-	assert.Equal(apis.Version(6), ver)
-	assert.Contains(err.Error(), "hello world 03")
+	assert.Error(t, err)
+	assert.Equal(t, apis.Version(6), ver)
+	assert.Contains(t, err.Error(), "hello world 03")
 }
 
 func TestChunkserver_StartWrite(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("StartWrite", 76, 61, []byte("phenomenologist")).Return(nil)
 	mocked.On("StartWrite", 0, 0, []byte(nil)).Return(errors.New("hello world 04"))
 
-	assert.NoError(server.StartWrite(76, 61, []byte("phenomenologist")))
+	assert.NoError(t, server.StartWrite(76, 61, []byte("phenomenologist")))
 
 	err := server.StartWrite(0, 0, []byte{})
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 04")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 04")
 }
 
 func TestChunkserver_CommitWrite(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("CommitWrite", 77, "this is my hash", 62, 63).Return(nil)
 	mocked.On("CommitWrite", 0, "", 0, 0).Return(errors.New("hello world 05"))
 
-	assert.NoError(server.CommitWrite(77, "this is my hash", 62, 63))
+	assert.NoError(t, server.CommitWrite(77, "this is my hash", 62, 63))
 
 	err := server.CommitWrite(0, "", 0, 0)
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 05")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 05")
 }
 
 func TestChunkserver_UpdateLatestVersion(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("UpdateLatestVersion", 78, 64, 65).Return(nil)
 	mocked.On("UpdateLatestVersion", 0, 0, 0).Return(errors.New("hello world 06"))
 
-	assert.NoError(server.UpdateLatestVersion(78, 64, 65))
+	assert.NoError(t, server.UpdateLatestVersion(78, 64, 65))
 
 	err := server.UpdateLatestVersion(0, 0, 0)
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 06")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 06")
 }
 
 func TestChunkserver_Add(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("Add", 79, []byte("quest"), 66).Return(nil)
 	mocked.On("Add", 0, []byte(nil), 0).Return(errors.New("hello world 07"))
 
-	assert.NoError(server.Add(79, []byte("quest"), 66))
+	assert.NoError(t, server.Add(79, []byte("quest"), 66))
 
 	err := server.Add(0, []byte{}, 0)
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 07")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 07")
 }
 
 func TestChunkserver_Delete(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("Delete", 80, 67).Return(nil)
 	mocked.On("Delete", 0, 0).Return(errors.New("hello world 08"))
 
-	assert.NoError(server.Delete(80, 67)) // TODO: purposefully wrong to test tests
+	assert.NoError(t, server.Delete(80, 67))
 
 	err := server.Delete(0, 0)
-	assert.Error(err)
-	assert.Contains(err.Error(), "hello world 08")
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "hello world 08")
 }
 
 func TestChunkserver_ListAllChunks_Pass(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("ListAllChunks").Return([]struct {
@@ -161,8 +159,8 @@ func TestChunkserver_ListAllChunks_Pass(t *testing.T) {
 	}, nil)
 
 	chunks, err := server.ListAllChunks()
-	assert.NoError(err)
-	assert.Equal([]struct {
+	assert.NoError(t, err)
+	assert.Equal(t, []struct {
 		Chunk   apis.ChunkNum
 		Version apis.Version
 	}{
@@ -171,7 +169,7 @@ func TestChunkserver_ListAllChunks_Pass(t *testing.T) {
 }
 
 func TestChunkserver_ListAllChunks_Fail(t *testing.T) {
-	assert, mocked, teardown, server := beginTest(t)
+	mocked, teardown, server := beginTest(t)
 	defer teardown()
 
 	mocked.On("ListAllChunks").Return([]struct {
@@ -181,10 +179,10 @@ func TestChunkserver_ListAllChunks_Fail(t *testing.T) {
 		errors.New("hello world 09"))
 
 	chunks, err := server.ListAllChunks()
-	if assert.Error(err) {
-		assert.Contains(err.Error(), "hello world 09")
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "hello world 09")
 	}
-	assert.Empty(chunks)
+	assert.Empty(t, chunks)
 }
 
 type MockChunkserver struct {
